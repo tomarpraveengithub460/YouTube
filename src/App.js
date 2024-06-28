4fe8b183046093f47e8899344f63e51f1a92e171
Praveen Tomar <praveentomar460@gmail.com>
2024-06-29 02:17:05 +0530

    Adding files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Body from './components/Body';
 function App() {
   return (
     <div className="App">
       <h1 className="font-bold text-green-600 text-center pt-10 text-4xl ">Hello You Tube</h1>
+      <Body />
     </div>
   );
 }
---
+++ src/components/VideoContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const VideoContainer=()=>{
+    return (
+        <div>
+            <h1>Video Container</h1>
+        </div>
+    )
+}
+export default VideoContainer;
